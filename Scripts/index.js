70baff9d26576b00a1054e740d25a59d2f2aea46
vitorbenedito <vitor.bsdc@gmail.com>
2021-06-25 00:23:57 -0300

    pequenas mundanças

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,16 +1,13 @@
-function insert(num) {
-   var numero = document.getElementById('resultado').innerHTML;
-   document.getElementById('resultado').innerHTML = numero + num;
-}
-
 function clean() {
-    document.getElementById('resultado').innerHTML = "";
-}
+    document.getElementById('resultado').innerHTML = ""; }
 
 function back() {
     var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
-}
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1); }
+
+function insert(num) {
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num; }
 
 function calcular() {
     var resultado = document.getElementById('resultado').innerHTML;
