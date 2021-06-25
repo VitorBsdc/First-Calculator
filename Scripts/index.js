b54a8369df3799365f5fab71eb80cbf2a261c5b3
vitorbenedito <vitor.bsdc@gmail.com>
2021-06-25 01:08:23 -0300

    comentários

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,20 +1,25 @@
 function clean() {
     document.getElementById('resultado').innerHTML = ""; }
+    /*Aspas representa null. Ou seja vai retirar tudo*/
 
 function back() {
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1); }
+    /*ZERO representa a posição que sera retirada -1*/
 
 function insert(num) {
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num; }
+    /*Número n++*/
 
 function calcular() {
     var resultado = document.getElementById('resultado').innerHTML;
     if(resultado) {
         document.getElementById('resultado').innerHTML = eval(resultado);
+        /*Eval utilizado para calcular resultado*/
     }
     else {
         document.getElementById('resultado').innerHTML = "Error!"
+        /*Mensagem caso esteja vazio resultado*/
     }
 }
